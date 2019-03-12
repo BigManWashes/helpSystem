1990a94d2c1de5804e3e3165a981a6c4c56e1af8
Adam Grant <writeadamg@gmail.com>
2019-03-12 17:06:20 -0500

    Switch to Big Man Branding

--- doczrc.js
+++ doczrc.js
@@ -1,3 +1,29 @@
 export default {
-    dest: '/dist'
+    dest: '/dist',
+    title: 'Big Man Help System',
+    description: 'Big Man Washes internal help system',
+    public: '/public',
+    themeConfig: {
+        logo: {
+            src: '/public/img/logo.png',
+            width: 232,
+            display: 'block',
+            margin: 'auto'
+        },
+        colors: {
+            primary: '#003366',
+            secondary: '#ffc237',
+            grey: '#323840g'
+        },
+        styles: {
+            h1: {
+                font: 'Calibri',
+                fontSize: '14',
+            },
+        },
+        files: [
+            "static/"
+        ]
+    }
+
 }
