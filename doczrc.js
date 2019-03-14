11a772273ad6f4b34200a6426cf97cb4d82dcea1
Adam Grant <writeadamg@gmail.com>
2019-03-14 18:04:58 -0500

    First round of docs

--- doczrc.js
+++ doczrc.js
@@ -1,37 +1,26 @@
 export default {
-    dest: '/dist',
-    title: 'Big Man Help System',
-    description: 'Big Man Washes internal help system',
-    public: '/public',
-    menu: [
-        'Home',
-        'Hydraulics',
-        {
-            name: 'Controls',
-            menu: [
-                'VFDs',
-                'Tunnel Controller'
-            ]
-        }
-    ],
+    dest: "/dist",
+    title: "Big Man Help System",
+    description: "Big Man Washes internal help system",
+    public: "/public",
+    menu: ["Home", "Hydraulics", "VFDs"],
     themeConfig: {
         logo: {
-            src: '/public/img/logo.png',
+            src: "/public/img/logo.png",
             width: 232,
-            display: 'block',
-            margin: 'auto'
+            display: "block",
+            margin: "auto"
         },
         colors: {
-            primary: '#003366',
-            secondary: '#ffc237',
-            grey: '#323840g'
+            primary: "#003366",
+            secondary: "#ffc237",
+            grey: "#323840g"
         },
         styles: {
             h1: {
-                font: 'Calibri',
-                fontSize: '14',
-            },
+                fontFamily: "Helvetica",
+                fontSize: "16"
+            }
         }
     }
-
-}
+};
